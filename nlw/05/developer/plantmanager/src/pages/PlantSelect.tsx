e78f7764eb4f36bd58d317541ee32bc8b1780314
Wanderson.Freitas <wandersonaraujofreitas@gmail.com>
2021-04-22 23:52:14 -0300

    developing app

--- nlw/05/developer/plantmanager/src/pages/PlantSelect.tsx
+++ nlw/05/developer/plantmanager/src/pages/PlantSelect.tsx
@@ -1,19 +1,162 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Text,
     StyleSheet,
     View,
+    FlatList,
+    ActivityIndicator
 } from 'react-native';
-import { Button } from '../components/Button';
+import { Header } from '../components/Header';
+import { EnvironmentButton } from "../components/EnvironmentButton";
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
+import { Load } from "../components/load";
+
+
+import api from '../service/api';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnvironmentProps {
+    key: string;
+    title: string;
+}
+
+interface PlantsProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+        times: number,
+        repeat_every: string
+    }
+}
+
 export function PlantSelect() {
+    const [environment, setEnvironment] = useState<EnvironmentProps[]>();
+    const [plants, setPlants] = useState<PlantsProps[]>();
+    const [filteredPlants, setFilteredPlants] = useState<PlantsProps[]>();
+    const [environmentSelected, setEnvironmentSelected] = useState('all');
+    const [loading, setLoading] = useState(true);
+
+    const [page, setPage] = useState(1);
+    const [loadingMore, setLoadingMore] = useState(true);
+    const [loadingAll, setLoadingAll] = useState(false);
+
+
+    function handleEnvironmentSelected(environment: string) {
+        setEnvironmentSelected(environment);  
+        
+        if (environment === 'all')
+            return setFilteredPlants(plants);
+            
+        const filtered = plants?.filter(plant => 
+                plant.environments.includes(environment)
+        );
+
+        setFilteredPlants(filtered);
+    }
+
+    async function fetchPlants() {
+        const { data } = await api
+            .get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+        
+        if (!data)
+            return setLoading(true);
+
+        if (page > 1) {
+            setPlants(oldValue => [ ...oldValue, ...data ]);
+            setFilteredPlants(oldValue => [ ...oldValue, ...data ]);
+        } else {
+            setPlants(data);
+            setFilteredPlants(data);
+        }
+        
+        setLoading(false);
+        setLoadingMore(false);
+    }
+
+    function handleFetchMore(distance: number){
+        if(distance < 0) 
+            return;
+        
+        setLoadingMore(true);
+        setPage(oldValue => oldValue + 1);
+        fetchPlants();
+    }
+
+    useEffect(() => {
+        async function fetchEnvironment() {
+            const { data } = await api
+                .get('plants_environments?_sort=title&_order=asc');
+            setEnvironment([
+                {
+                    key: 'all',
+                    title: 'Todos'
+                },
+                ...data
+            ]);
+        }
+
+        fetchEnvironment();
+    }, [])
+
+    useEffect(() => {
+        fetchPlants();
+    }, [])
+
+    if (loading) {
+        return <Load />
+    }
     return (
         <View style={styles.container}>
-            <Text>Selecionar planta</Text>
+            <View style={styles.header}>
+                <Header />
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subtitle}>
+                    você quer colocar sua planta?
+                </Text>
+            </View>
+
+            <View>
+                <FlatList
+                    data={environment}
+                    renderItem={({ item }) => (
+                        <EnvironmentButton 
+                            title={item.title}
+                            active={item.key === environmentSelected}
+                            onPress={() => handleEnvironmentSelected(item.key)}
+                        />
+                    )}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.environmentaList}
+                    />
+            </View>
+            <View style={styles.plants}>
+                <FlatList
+                    data={filteredPlants}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary 
+                            data={item} />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                    onEndReachedThreshold={0.1}
+                    onEndReached={({ distanceFromEnd }) => handleFetchMore(distanceFromEnd)}
+                    ListFooterComponent={
+                        loadingMore ?
+                        <ActivityIndicator color={colors.green}/>
+                        : <></>
+                    }
+                    />
+            </View>
         </View>
     )
 }
@@ -21,8 +164,34 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: colors.background
-    }
+    },
+    header:{
+        paddingHorizontal: 30,
+    },
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontSize: 17,
+        fontFamily: fonts.text,
+        lineHeight: 20,
+        color: colors.heading,
+    },
+    environmentaList: {
+        height: 40,
+        justifyContent: 'center',
+        paddingBottom: 5,
+        marginLeft: 32,
+        marginVertical: 32,
+    },
+    plants:{
+        flex: 1,
+        paddingHorizontal: 32,
+        justifyContent: 'center',
+    },
 });
